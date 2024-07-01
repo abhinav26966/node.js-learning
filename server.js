459f5fca4f0e934dfead11f8fedd0fb7d564b13a
Abhinav Nagar <abhinavnagar26966@gmail.com>
2024-07-01 11:50:59 +0530

    Servering index-html

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
-    res.write('<html><head><title>Node.js Class</title></head><body>');
-
+    
     if (req.url === '/login') {
+        res.write('<html><head><title>Node.js Class</title></head><body>');
         res.write('<h1>Hello, Login!</h1>');
+        res.write('</body></html>');
+        res.end();
     } else {
-        res.write('<h1>Hello, World!</h1>');
+        const filePath = path.join(__dirname, 'index.html');
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.write('Internal Server Error');
+                res.end();
+                return;
+            }
+            res.write(data);
+            res.end();
+        });
     }
-
-    res.write('</body></html>');
-    res.end();
 });
 
 const port = 3000;
